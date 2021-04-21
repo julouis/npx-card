78e3c3325392fe89e108d59d7e08337b507d0396
Julien Louis <julienlouis.becode@gmail.com>
2021-04-21 22:42:47 +0200

    update

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 #!/usr/bin/env node
 
-const termImg = require('term-img');
 const chalk = require('chalk');
 const boxen = require('boxen');
-const cliBoxes = require('cli-boxes');
+const terminalImage = require('terminal-image');
 
-content = {
-    Name: "Julien Louis",
-    Work: "Junior at BeCode.org",
-    GitHub: "https://github.com/julouis",
-    LinkedIn: "https://www.linkedin.com/in/julien-louis/"
+data = {
+    name: chalk.blue("Julien Louis"),
+    work: "Junior at BeCode.org",
+    gitHub: "https://github.com/julouis",
+    linkedIn: "https://www.linkedin.com/in/julien-louis/"
 };
 
+
+(async (image) => {
+    image = await terminalImage.file('petit-poney-resize.jpg', {width:'50%', height: '50%'});
+    console.log(image);
+})();
+
+
+
 console.log(
-    boxen([
-        `${content.Name}`,
-        `${content.Work}`,
-    ].join(NEWLINE),
+    boxen(
+        [
+            `${data.name}`,
+            "",
+            `${chalk.underline("Work")}: ${data.work}`,
+            `${chalk.underline("GitHub")}: ${data.gitHub}`,
+            `${chalk.underline("LinkedIn")}: ${data.linkedIn}`,
+            
+        ].join("\n"),
         {padding: 3, margin: 3, borderStyle: 'double', borderColor: 'cyan'}))
 
 
